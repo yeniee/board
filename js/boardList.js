d2b2f1dbbae9f7247d0d60550479b1f241879d5c
yeniee <choeyeeun@naver.com>
2024-04-22 16:48:19 +0900

    [2024.04.22] index boardList css 수정

--- js/boardList.js
+++ js/boardList.js
@@ -55,3 +55,35 @@ function paging(){
         }) //i for end
     }
 }
+
+//scroll src
+
+
+let mobileHeaderSectionTop = document.querySelector(".mobile-header-section").offsetTop;
+let mobileHeaderSection = document.querySelector(".mobile-header-section")
+
+
+window.addEventListener("scroll",function(){
+  let scrollY = window.scrollY;
+  //console.log(scrollY);
+  if(scrollY>mobileHeaderSectionTop+50){
+    mobileHeaderSection.classList.add("on")
+  }
+  else{
+    mobileHeaderSection.classList.remove("on")
+  }
+
+})
+// pc scroll
+
+let pcHeaderSectionTop = document.querySelector(".pc-header-section").offsetTop;
+let pcHeaderSection = document.querySelector(".pc-header-section")
+
+window.addEventListener("scroll",function(){
+    if(scrollY>pcHeaderSectionTop+50){
+        pcHeaderSection.classList.add("on")
+      }
+      else{
+        pcHeaderSection.classList.remove("on")
+      }
+})//end
